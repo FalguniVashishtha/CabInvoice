94f1290f361245794b83c19ce8fc8789e192e2ee
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-09-12 00:09:39 +0530

    Multiple Rides

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 /**
  * Creating CabInvoiceGenerator class for invoice generator for the given distance and time
  */
-
 public class CabInvoiceGenerator {
     private static final int MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
@@ -16,6 +15,21 @@ public class CabInvoiceGenerator {
      */
     public static double calculateFare(double distance, int time) {
         return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+    }
+
+    /**
+     * calculate the aggregatetotal for all
+     * @param rides
+     * @return
+     */
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
     /**
      * Main method to print Welcome message
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -1,14 +1,43 @@
 package com.bridgelabz;
 import org.junit.*;
-//import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+    /**
+     * test for calculate fare *UC1
+     */
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    /**
+     * test for minimum input
+     */
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(2, fare, 0.0);
+    }
+
+    /**
+     * test for multiple rides input *UC2
+     */
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double totalFare = cabInvoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(27.0, totalFare, 0.0);
     }
 }
